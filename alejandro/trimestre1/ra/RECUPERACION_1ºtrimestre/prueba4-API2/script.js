0c2377242d95a535ca641ac277fe4d746fc32735
alejandro <arodriguezi10@educarex.es>
2026-06-07 16:18:16 +0200

    Ejercicio de 2 de api

---
+++ alejandro/trimestre1/ra/RECUPERACION_1ºtrimestre/prueba4-API2/script.js
@@ -0,0 +1,30 @@
+//recogemos los elementos del DOM
+const btnCargar = document.getElementById('btnCargar');
+const contenedorPosts = document.getElementById('contenedorPosts');
+
+//evento para el boton
+btnCargar.addEventListener('click', obtenerPost);
+
+function obtenerPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(respuesta => respuesta.json())
+
+    .then(publicaciones => {
+      let html = '';
+
+      const posts = publicaciones.slice(0, 5);
+
+      posts.forEach(p => {
+        html += `<p><strong>${p.title}</strong></p>`;
+      });
+
+      contenedorPosts.innerHTML = html;
+    })
+
+    .catch(error => {
+      console.error("Error al mostrar los datos: ", error);
+      contenedorPost.innerHTML = '<p>Ocurrió un error al cargar.</p>';
+    });
+}
+
+
